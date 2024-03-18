f12b26064a73ab47af23ab3369bf315870590024
griffingreenwood21 <141801624+griffingreenwood21@users.noreply.github.com>
2024-03-18 08:25:30 -0600

    completed architecture for thunderbird_fsm.vhd

--- src/hdl/thunderbird_fsm.vhd
+++ src/hdl/thunderbird_fsm.vhd
@@ -11,8 +11,8 @@
 --| ---------------------------------------------------------------------------
 --|
 --| FILENAME      : thunderbird_fsm.vhd
---| AUTHOR(S)     : Capt Phillip Warner, Capt Dan Johnson
---| CREATED       : 03/2017 Last modified 06/25/2020
+--| AUTHOR(S)     : C3C Griffin Greenwood
+--| CREATED       : 03/18/2024
 --| DESCRIPTION   : This file implements the ECE 281 Lab 2 Thunderbird tail lights
 --|					FSM using enumerated types.  This was used to create the
 --|					erroneous sim for GR1
@@ -36,18 +36,18 @@
 --|					can be changed by the inputs
 --|					
 --|
---|                 xxx State Encoding key
+--|                 One-Hot State Encoding key
 --|                 --------------------
 --|                  State | Encoding
 --|                 --------------------
---|                  OFF   | 
---|                  ON    | 
---|                  R1    | 
---|                  R2    | 
---|                  R3    | 
---|                  L1    | 
---|                  L2    | 
---|                  L3    | 
+--|                  OFF   | 10000000
+--|                  ON    | 01000000
+--|                  R1    | 00100000
+--|                  R2    | 00010000
+--|                  R3    | 00001000
+--|                  L1    | 00000100
+--|                  L2    | 00000010
+--|                  L3    | 00000001
 --|                 --------------------
 --|
 --|
@@ -85,24 +85,54 @@ library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
  
-entity thunderbird_fsm is 
-  port(
-	
-  );
-end thunderbird_fsm;
+entity thunderbird_fsm is
+      port (
+          i_clk, i_reset  : in    std_logic;
+          i_left, i_right : in    std_logic;
+          o_lights_L      : out   std_logic_vector(2 downto 0);
+          o_lights_R      : out   std_logic_vector(2 downto 0)
+      );
+  end thunderbird_fsm;
 
 architecture thunderbird_fsm_arch of thunderbird_fsm is 
-
--- CONSTANTS ------------------------------------------------------------------
+	-- create register signals with default state yellow (10)
+	signal f_Q : std_logic_vector (7 downto 0) := "10";
+	signal f_Q_next : std_logic_vector (7 downto 0) := "10";
   
 begin
 
-	-- CONCURRENT STATEMENTS --------------------------------------------------------	
+	-- CONCURRENT STATEMENTS ----------------------------
+	-- Next state logic
+	f_Q_next(7) <= (f_Q(7) and not i_left and not i_right) or f_Q(6) or f_Q(3) or f_Q(0);
+	f_Q_next(6) <= f_Q(7) and i_left and i_right;
+	f_Q_next(5) <= f_Q(7) and not i_left and i_right;
+	f_Q_next(4) <= f_Q(5);
+	f_Q_next(3) <= f_Q(4);
+	f_Q_next(2) <= f_Q(7) and i_left and not i_right;
+	f_Q_next(1) <= f_Q(2);
+	f_Q_next(0) <= f_Q(1);
 	
-    ---------------------------------------------------------------------------------
+	-- Output logic
+    o_lights_L(2) <= f_Q(6) or f_Q(0);
+    o_lights_L(1) <= f_Q(6) or f_Q(1) or f_Q(0);
+    o_lights_L(0) <= f_Q(6) or f_Q(2) or f_Q(1) or f_Q(0);
+    o_lights_R(2) <= f_Q(6) or f_Q(3);
+    o_lights_R(1) <= f_Q(6) or f_Q(4) or f_Q(3);
+    o_lights_R(0) <= f_Q(6) or f_Q(5) or f_Q(4) or f_Q(3);
+	-------------------------------------------------------	
 	
-	-- PROCESSES --------------------------------------------------------------------
-    
-	-----------------------------------------------------					   
+	-- PROCESSES ----------------------------------------	
+    -- state memory w/ asynchronous reset ---------------
+    register_proc : process (i_clk, i_reset)
+    begin
+            --Reset state is yellow
+            if i_reset = '1' then
+                 f_Q <= "10";
+            elsif (rising_edge(i_clk)) then
+                 f_Q <= f_Q_next;
+            end if;
+
+    end process register_proc;
+    -------------------------------------------------------				   
 				  
 end thunderbird_fsm_arch;
